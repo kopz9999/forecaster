5dbf07a82145274b44b09172089958f743bae421
Luis Canales <kopz9999@gmail.com>
2022-01-22 12:51:40 -0800

    cached postals

--- app/services/WeatherService.rb
+++ app/services/WeatherService.rb
@@ -3,8 +3,17 @@ require 'net/http'
 
 class WeatherService
   SERVICE_URL = 'http://api.weatherapi.com/v1/current.json'
+  include Singleton
 
-  def self.call(postal_code)
+  def call(postal_code)
+    Rails.cache.fetch("weather_codes_#{postal_code}", expires_in: 30.minutes) do
+      do_call postal_code
+    end
+  end
+
+  private
+
+  def do_call(postal_code)
     uri = URI("#{SERVICE_URL}?key=#{ENV['WEATHER_API_KEY']}&q=#{postal_code}&aqi=no")
     res = Net::HTTP.get_response(uri)
     if res.is_a?(Net::HTTPSuccess)
